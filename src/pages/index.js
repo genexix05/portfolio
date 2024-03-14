8aa384184994a05c35f190caa4e9b207f7a817b8
genexix05 <genexix05@gmail.com>
2024-03-14 16:54:21 +0100

    Agrega contenido a la página de inicio

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div className="container mx-auto">
+            <h1 className="text-5xl text-center mt-20">Bienvenido a Mi Portafolio</h1>
+            // Más contenido aquí
+        </div>
+    );
+}
