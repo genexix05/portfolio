c9171ecbc345abd8466b96f5019fc69c7e23e8db
genexix05 <genexix05@gmail.com>
2024-03-14 18:50:06 +0100

    Actualizar importación del logo en el encabezado

--- components/header.js
+++ components/header.js
@@ -1,11 +1,13 @@
 import Link from 'next/link';
-import Logo from '../public/logo.svg'; // Importa el SVG
+import Image from 'next/image'; 
 
 function Header() {
     return (
         <header className="fixed top-5 left-1/2 transform -translate-x-1/2 w-5/6 px-5 py-1 flex justify-center items-center backdrop-blur-lg bg-white/30 z-50 rounded-lg border border-gray-200">
             <nav className="container flex justify-between items-center py-4">
-                <Logo className="h-6 w-6 text-blue-500" />
+                <div className="h-6 w-6 text-blue-500">
+                    <Image src="../public/logo.svg" alt="Logo" width={24} height={24} />
+                </div>
                 <div className="flex items-center space-x-4">
                     <Link href="/pages" className="hover:text-blue-500">Pages</Link>
                     <Link href="/account" className="hover:text-blue-500">Account</Link>
